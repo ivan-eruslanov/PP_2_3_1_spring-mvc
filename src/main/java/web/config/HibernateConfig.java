96c15908b646bd4576f6cac85f98081417ce1521
eruslanov-ivan <iv.eruslanov@yandex.ru>
2022-08-11 23:11:27 +0500

    fixed

--- src/main/java/web/config/HibernateConfig.java
+++ src/main/java/web/config/HibernateConfig.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
+import java.util.Objects;
 import java.util.Properties;
 
 @Configuration
@@ -31,7 +32,7 @@ public class HibernateConfig {
     @Bean
     public DataSource getDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("db.driver"));
+        dataSource.setDriverClassName(Objects.requireNonNull(env.getProperty("db.driver")));
         dataSource.setUrl(env.getProperty("db.url"));
         dataSource.setUsername(env.getProperty("db.username"));
         dataSource.setPassword(env.getProperty("db.password"));
@@ -51,7 +52,7 @@ public class HibernateConfig {
         LocalContainerEntityManagerFactoryBean bean
                 = new LocalContainerEntityManagerFactoryBean();
         bean.setDataSource(getDataSource());
-        bean.setPackagesToScan(new String[]{"web.model"});
+        bean.setPackagesToScan("web.model");
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         bean.setJpaVendorAdapter(vendorAdapter);
         bean.setJpaProperties(getHibernateProperties());
